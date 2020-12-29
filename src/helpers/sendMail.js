8a8ef2954654d48da823a296342ff699cd42f44a
Ibrat Nawaz <ibratrocker99@gmail.com>
2020-12-29 22:22:10 +0530

    change to heroku url

--- src/helpers/sendMail.js
+++ src/helpers/sendMail.js
@@ -30,7 +30,7 @@ async function sendMail(user, type) {
         subject: `Activate your account`,
         html: `<h3>Good to see you here ${first_name} ${last_name}.</h3><br/>
             <p>Please click the below link to activate your account and enjoy our services.</p>
-            <p>http://localhost:5000/api/users/account/activate/${str}</p>`,
+            <p>https://m-drive-api.herokuapp.com/api/users/account/activate/${str}</p>`,
       });
     }
 
@@ -41,7 +41,7 @@ async function sendMail(user, type) {
         subject: `Reset Password`,
         html: `<h3>Hi ${first_name} ${last_name}</h3><br/>
             <p>Here's your reset password link.</p>
-            <p>http://localhost:5000/api/users/forgot/password/redirect/${_id}</p>`,
+            <p>https://m-drive-api.herokuapp.com/api/users/forgot/password/redirect/${_id}</p>`,
       });
     }
   } catch (error) {
